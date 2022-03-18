b83a9c5733d36349a97efd250c6127785ed938d3
BansalShefali <shefali.bansal2008@gmail.com>
2022-03-19 00:01:41 +0530

    overhaul About Page
    intro, meth...pending results

--- index.js
+++ index.js
@@ -37,3 +37,18 @@ $(document).ready(function(){
 	  $('.toggle').toggleClass('active')
 	})
   })
+
+//Try
+
+$(document).ready(function() {
+    $("#news-slider").owlCarousel({
+        items : 3,
+        itemsDesktop:[1199,3],
+        itemsDesktopSmall:[980,2],
+        itemsMobile : [600,1],
+        navigation:true,
+        navigationText:["",""],
+        pagination:true,
+        autoPlay:true
+    });
+});
